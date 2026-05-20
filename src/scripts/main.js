c01548c6acf0ccdbc549e884793362051b2a26c0
Kateryna Berdnikova <berdekaterinasp@gmail.com>
2026-05-20 14:40:38 +0300

    solution5

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,12 @@
 'use strict';
+
+const form = document.querySelector('.subscribe__form');
+
+form.addEventListener('submit', (e) => {
+  e.preventDefault();
+
+  const input = form.querySelector('input');
+
+  input.value = '';
+  console.log('JS работает');
+});
